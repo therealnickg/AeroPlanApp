700c7bfe5eca1c7b0c946eb10a2ae8cad73582fe
therealnickg <nguardadoguardo@gmail.com>
2023-08-31 21:38:08 -0700

    Initial Commit

---
+++ AeroPlanApp/AeroPlanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AeroPlanAppApp.swift
+//  AeroPlanApp
+//
+//  Created by Nicolas Guardado Guardado on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct AeroPlanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AeroPlanApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AeroPlanApp
+//
+//  Created by Nicolas Guardado Guardado on 8/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
