c5928475ad40b7df511f37d91776042f30c6545c
Aaron Yu <48599146+ayu0401@users.noreply.github.com>
2023-09-05 12:55:25 -0700

    created The Pilot Review Notes
    starting on the development of the Pilot review function

---
+++ AeroPlanApp/PilotReviewNotesView.swift
@@ -0,0 +1,33 @@
+//
+//  PilotReviewNotesView.swift
+//  AeroPlanApp
+//
+//  Created by Aaron Yu on 9/5/23.
+//
+
+import SwiftUI
+
+struct PilotReviewNotesView: View {
+    var body: some View {
+        VStack{
+            Text("Pilot Review Notes")
+                .font(.largeTitle)
+                .foregroundColor(.red)
+                .padding(.top)
+            Spacer()
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+}
+
+struct PilotReviewNotesView_Previews: PreviewProvider {
+    static var previews: some View {
+        PilotReviewNotesView()
+    }
+}
