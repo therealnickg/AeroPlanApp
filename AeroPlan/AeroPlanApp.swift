e996e20aad183049cd7d4addbecda8fac87682a7
Nguyen <xyonguyen@gmail.com>
2023-10-03 23:02:37 -0700

    Initial Commit

---
+++ AeroPlan/AeroPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  AeroPlanApp.swift
+//  AeroPlan
+//
+//  Created by Nguyen Vo on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct AeroPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AeroPlan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AeroPlan
+//
+//  Created by Nguyen Vo on 10/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
