ed2525dd3e8f3e822e4a3106cfeabfeb9d2379a0
geodude1992 <oblivichiiipower@gmail.com>
2023-11-07 00:31:35 -0800

    METAR near final build

---
+++ AeroPlanApp/TafView.swift
@@ -0,0 +1,18 @@
+//
+//  TafView.swift
+//  AeroPlanApp
+//
+//  Created by Jovanni Garcia on 11/6/23.
+//
+
+import SwiftUI
+
+struct TafView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TafView()
+}
