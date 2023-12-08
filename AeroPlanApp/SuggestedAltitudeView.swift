5f57b953913d0488b949ef9d93832474a6615611
therealnickg <nguardadoguardo@gmail.com>
2023-12-08 00:41:42 -0800

    pushing suggestAltitude feature!

---
+++ AeroPlanApp/SuggestedAltitudeView.swift
@@ -0,0 +1,18 @@
+//
+//  SuggestedAltitudeView.swift
+//  AeroPlanApp
+//
+//  Created by Nicolas Guardado Guardado on 11/14/23.
+//
+
+import SwiftUI
+
+struct SuggestedAltitudeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SuggestedAltitudeView()
+}
