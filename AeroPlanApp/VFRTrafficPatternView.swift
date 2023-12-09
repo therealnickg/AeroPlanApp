766d840589bbb0e44d4c87d7f43dcc6d223a4ef7
therealnickg <nguardadoguardo@gmail.com>
2023-12-08 22:10:48 -0800

    probably one of the last commits to my branch ever

---
+++ AeroPlanApp/VFRTrafficPatternView.swift
@@ -0,0 +1,18 @@
+//
+//  VFRTrafficPatternView.swift
+//  AeroPlanApp
+//
+//  Created by Nicolas Guardado Guardado on 12/8/23.
+//
+
+import SwiftUI
+
+struct VFRTrafficPatternView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VFRTrafficPatternView()
+}
